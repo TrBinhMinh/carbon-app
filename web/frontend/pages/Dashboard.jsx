60af298a50ab0fc1c24959f8b35cb32c230eeaa7
TrBinhMinh <bminh9900@gmail.com>
2023-05-02 22:07:10 +0700

    update new UI

--- web/frontend/pages/Dashboard.jsx
+++ web/frontend/pages/Dashboard.jsx
@@ -1,51 +1,98 @@
-import { AlphaCard, Page, Layout, VerticalStack, Text } from "@shopify/polaris";
+import {
+  AlphaCard,
+  Page,
+  Layout,
+  VerticalStack,
+  Text,
+  HorizontalStack,
+} from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
 export default function PageName() {
-  const primaryAction = { content: "Foo", url: "/foo" };
-  const secondaryActions = [{ content: "Bar", url: "/bar" }];
+  const primaryAction = { content: "Primary action", url: "/foo" };
+  const secondaryActions = [
+    { content: "Edit", url: "/bar" },
+    { content: "Refund", url: "/bar" },
+  ];
   const actionGroups = [
-    { title: "Baz", actions: [{ content: "Baz", url: "/baz" }] },
+    { title: "More actions", actions: [{ content: "Baz", url: "/baz" }] },
   ];
 
   return (
-    <Page>
-      <TitleBar
-        title="Dashboard"
-        primaryAction={primaryAction}
-        secondaryActions={secondaryActions}
-        actionGroups={actionGroups}
-      />
+    <Page fullWidth={true}>
       <Layout>
         <Layout.Section>
-          <AlphaCard sectioned>
-            <Text variant="headingMd" as="h2">
-              Heading
-            </Text>
-            <VerticalStack>
-              <p>Body</p>
-            </VerticalStack>
-          </AlphaCard>
-          <AlphaCard sectioned>
-            <Text variant="headingMd" as="h2">
-              Heading
-            </Text>
-            <VerticalStack>
-              <p>Body</p>
-            </VerticalStack>
-          </AlphaCard>
+          <Text variant="heading3xl" as="h1" fontWeight="medium">
+            Welcome back, Tracy
+          </Text>
+          <Text variant="headingXl" as="h2" fontWeight="medium">
+            Let's see your impact!
+          </Text>
         </Layout.Section>
-        <Layout.Section secondary>
-          <AlphaCard sectioned>
-            <Text variant="headingMd" as="h2">
-              Heading
-            </Text>
-            <VerticalStack>
-              <p>Body</p>
-            </VerticalStack>
-          </AlphaCard>
+        <Layout.Section>
+          <HorizontalStack wrap={false} gap="4">
+            <AlphaCard sectioned>
+              <Text variant="headingMd" as="h3">
+                Heading
+              </Text>
+              <VerticalStack>
+                <p>Body</p>
+              </VerticalStack>
+            </AlphaCard>
+            <AlphaCard sectioned>
+              <Text variant="headingMd" as="h3">
+                Heading
+              </Text>
+              <VerticalStack>
+                <p>Body</p>
+              </VerticalStack>
+            </AlphaCard>
+          </HorizontalStack>
         </Layout.Section>
       </Layout>
     </Page>
+    // <Page
+    //   pagination={{
+    //     hasPrevious: true,
+    //     hasNext: true,
+    //   }}
+    // >
+    //   <TitleBar
+    //     title="Dashboard"
+    //     primaryAction={primaryAction}
+    //     secondaryActions={secondaryActions}
+    //     actionGroups={actionGroups}
+    //   />
+    //   <Layout>
+    //     <Layout.Section>
+    //       <AlphaCard sectioned>
+    //         <Text variant="headingMd" as="h2">
+    //           Heading
+    //         </Text>
+    //         <VerticalStack>
+    //           <p>Body</p>
+    //         </VerticalStack>
+    //       </AlphaCard>
+    //       <AlphaCard sectioned>
+    //         <Text variant="headingMd" as="h2">
+    //           Heading
+    //         </Text>
+    //         <VerticalStack>
+    //           <p>Body</p>
+    //         </VerticalStack>
+    //       </AlphaCard>
+    //     </Layout.Section>
+    //     <Layout.Section secondary>
+    //       <AlphaCard sectioned>
+    //         <Text variant="headingMd" as="h2">
+    //           Heading
+    //         </Text>
+    //         <VerticalStack>
+    //           <p>Body</p>
+    //         </VerticalStack>
+    //       </AlphaCard>
+    //     </Layout.Section>
+    //   </Layout>
+    // </Page>
   );
 }
